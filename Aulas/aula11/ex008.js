7d2db09f5b7d7e63e339e7ee5d0c6a0cf3876599
Matheus Luiz <99504258+matheusluizsouza@users.noreply.github.com>
2024-02-07 20:47:36 -0300

    update_07.02.24

--- Aulas/aula11/ex008.js
+++ Aulas/aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou corretamente!')
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
