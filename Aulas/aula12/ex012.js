2506035376eb470213d322e102ba6a24aad3c028
Matheus Luiz <99504258+matheusluizsouza@users.noreply.github.com>
2024-02-08 22:31:45 -0300

    update_caderno_08.02.24

---
+++ Aulas/aula12/ex012.js
@@ -0,0 +1,10 @@
+var hora = 22
+
+console.log(`São ${hora} horas!`)
+if (hora > 6 && hora < 13) {
+    console.log('Bom dia!')
+} else if (hora > 12 && hora < 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
