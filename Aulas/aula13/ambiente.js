38f655a182bab70d25e0314a29d1522f83063f04
Matheus Luiz <99504258+matheusluizsouza@users.noreply.github.com>
2024-02-27 18:25:02 -0300

    update_curso_27.02.24

---
+++ Aulas/aula13/ambiente.js
@@ -0,0 +1,26 @@
+var cont = 0
+
+while (cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont++ // A variável cont vai receber +1 a cada laço. (cont = cont + 1)
+}
+
+
+/*
+var cont = 0
+
+do {
+    console.log(`Passo ${cont}`)
+    cont++ // A variável cont vai receber +1 a cada laço. (cont = cont + 1)
+} while (cont <= 6)
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
