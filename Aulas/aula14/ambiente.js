30bca89916b52232157b30b1bc2914a51b7fe9bb
Matheus Luiz <99504258+matheusluizsouza@users.noreply.github.com>
2024-02-28 17:45:22 -0300

    update_curso_28.02.24

---
+++ Aulas/aula14/ambiente.js
@@ -0,0 +1,16 @@
+console.log('Vamos começar...')
+for(var cont = 0; cont<=6; cont++) {
+    console.log(`Passo ${cont}`)
+}
+console.log('FIM!')
+
+// Mesma execução que o código do exercicio anterior abaixo.
+
+/*
+var cont = 0
+
+while (cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont++ 
+}
+*/
