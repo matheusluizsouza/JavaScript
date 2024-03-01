83525330336f563f62b20f3f59b0c208cd867f29
Matheus Luiz <99504258+matheusluizsouza@users.noreply.github.com>
2024-02-29 21:35:04 -0300

    update_curso_29.02.24

--- Aulas/aula14ex/ex016/script.js
+++ Aulas/aula14ex/ex016/script.js
@@ -1,14 +1,31 @@
 function contar() {
-    var start = document.getElementById('numIni')
-    var end = document.getElementById('numFim')
-    var pass = document.getElementById('numPass')
-    var res = document.querySelector('div#res')
+    let start = document.getElementById('numIni')
+    let end = document.getElementById('numFim')
+    let pass = document.getElementById('numPass')
+    let res = document.querySelector('div#res')
+    
 
-        if (start.value == '' || end.value == '') {
-            res.innerHTML = 'Impossivel Contar!'
-        } else if (pass.value == 0 || pass.value == '') {
-            window.alert('Impossivel realizar a contagem com o valor de Passo igual a 0!')
-        } else {
-            res.innerHTML = 'Contando:...'
+    if (start.value.length == 0 || end.value.length == 0 || pass.value.length == 0) {
+        res.innerHTML = 'Impossivel Contar!'
+    } else {
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(start.value)
+        let f = Number(end.value)
+        let p = Number(pass.value)
+        if (p <= 0) {
+            window.alert('[ERRO] O valor do Passo não pode ser igual a zero! Por favor, preencha novamente.')
         }
+        if (f > i) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} ` // Utilizar somente o = e ñ += será exibido apenas o resultado final da contagem e nao ela inteira
+            }
+        } else if (f < i) {
+            // Contagem decrescente
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} ` 
+            }
+        }
+        res.innerHTML += `\u{2705}` // Acrescenta o CHECK verde ao final de cada condiçao acima
+    }
 }
